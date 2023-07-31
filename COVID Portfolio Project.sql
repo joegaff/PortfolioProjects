766d26ed1ab5e3649fdb83c4117b8b3bd9f2efd5
joegaff <139297533+joegaff@users.noreply.github.com>
2023-07-31 13:22:15 -0400

    Update COVID Portfolio Project.sql

--- COVID Portfolio Project.sql
+++ COVID Portfolio Project.sql
@@ -1,65 +1,77 @@
--- Data
-SELECT Location, date, total_cases, new_cases, total_deaths, population
-FROM Portfolio_Project.coviddeaths
-ORDER BY 1,2;
+-- Global Numbers: Total Cases, Total Deaths, Overall Death Rate
+SELECT 
+    SUM(new_cases) AS total_cases,
+    SUM(new_deaths) AS total_deaths,
+    SUM(new_deaths) / SUM(new_cases) * 100 AS Death_Rate
+FROM
+    Portfolio_Project.coviddeaths
+WHERE
+    continent <> ''
+ORDER BY 1 , 2;
 
--- Looking at Total Cases vs Total Deaths
--- Death Rate: Likelihood of Dying from Covid
-SELECT Location, date, total_cases, total_deaths, (total_deaths/total_cases) * 100 as Death_Rate
-FROM Portfolio_Project.coviddeaths
-WHERE location = 'United States' and total_cases <> 0
-ORDER BY 1,2;
+-- Looking at Total Cases, Total Deaths, and Death Rates per day in United States
+SELECT 
+    Location,
+    date,
+    total_cases,
+    total_deaths,
+    (total_deaths / total_cases) * 100 AS Death_Rates
+FROM
+    Portfolio_Project.coviddeaths
+WHERE
+    location = 'United States'
+        AND total_cases <> 0
+ORDER BY 1 , 2;
 
--- Looking at Total Cases vs Population
--- Population_Infected_Rate: Percentage of Population infected from Covid at some point
-SELECT Location, date, population, total_cases, (total_cases/population) * 100 as Population_Infected_Rate
-FROM Portfolio_Project.coviddeaths
-WHERE location = 'United States' and total_cases <> 0
-ORDER BY 1,2;
+-- Looking at Total Percent of Population that has been infected by COVID on a day-to-day bias
+SELECT 
+    Location,
+    date,
+    population,
+    total_cases,
+    (total_cases / population) * 100 AS Population_Infected_Rate
+FROM
+    Portfolio_Project.coviddeaths
+WHERE
+    location = 'United States'
+        AND total_cases <> 0
+ORDER BY 1 , 2;
 
--- Looking at Countries with the Highest Infection_Rate compared to Population
-SELECT Location, population, max(total_cases) as Highest_Infection_Count, Max((total_cases/population)) * 100 as Population_Infected_Rate
-FROM Portfolio_Project.coviddeaths
-GROUP BY Location, Population
-ORDER BY Population_Infected_Rate desc;
+-- Looking at Top 10 Countries with the Highest Infection Rates compared to Population
+SELECT 
+    Location,
+    population,
+    MAX(total_cases) AS Highest_Infection_Count,
+    MAX((total_cases / population)) * 100 AS Population_Infected_Rate
+FROM
+    Portfolio_Project.coviddeaths
+GROUP BY Location , Population
+ORDER BY Population_Infected_Rate DESC
+LIMIT 10;
 
--- Looking at Countries with the Highest Death_Rate
-SELECT Location, max(total_deaths) as Total_Death_Count
-FROM Portfolio_Project.coviddeaths
-WHERE continent <> ''
+-- Looking at Top 10 Countries with the Highest Death Count
+SELECT 
+    Location, MAX(total_deaths) AS Total_Death_Count
+FROM
+    Portfolio_Project.coviddeaths
+WHERE
+    continent <> ''
 GROUP BY Location
-ORDER BY Total_Death_Count desc;
+ORDER BY Total_Death_Count DESC
+LIMIT 10;
 
--- Breaking Down by Continent
-
--- Showing continents with the highest death count per population
-SELECT location, max(total_deaths) as Total_Death_Count
-FROM Portfolio_Project.coviddeaths
-WHERE continent = '' AND location not like "%income%" 
+SELECT 
+    location, MAX(total_deaths) AS Total_Death_Count
+FROM
+    Portfolio_Project.coviddeaths
+WHERE
+    continent = ''
+        AND location NOT LIKE '%income%'
+        AND location NOT LIKE '%World%'
 GROUP BY location
-ORDER BY Total_Death_Count desc;
+ORDER BY Total_Death_Count DESC;
 
--- Global Numbers
-
--- Total Cases, Total Deaths, Overall Death Rate
-SELECT SUM(new_cases) as total_cases, SUM(new_deaths) as total_deaths, SUM(new_deaths)/sum(new_cases) *100 as Death_Rate
-FROM Portfolio_Project.coviddeaths
-WHERE continent <> '' 
-ORDER BY 1,2;
-
-
--- Looking at Total Population vs Vaccinations
-Select DEATH.continent, DEATH.location, DEATH.date, DEATH.population, VAC.new_vaccinations
-,SUM(VAC.new_vaccinations) OVER (Partition by DEATH.location Order by DEATH.location, DEATH.date) as rolling_total_vaccinations
-From Portfolio_Project.CovidDeaths as DEATH
-Join Portfolio_Project.CovidVaccinations as VAC
-	on DEATH.location = VAC.location
-    and DEATH.date = VAC.date
-WHERE DEATH.continent <> '' 
-order by 2,3;
-
--- Using CTE
-
+-- Looking at the Rolling Total Vaccinations and Percent of Population Vaxxed per Continent per day
 With PopvsVac (continent, location, date, population, new_vaccinations, rolling_total_vaccinations)
 as
 (
@@ -75,7 +87,7 @@ order by 2,3
 Select *, (Rolling_total_vaccinations/population) * 100 as Population_Vaxed_Percentage
 From PopvsVac;
 
--- Which country is most vaccinated?
+-- Which country is most vaccinated per pupil?
 With PopvsVac (continent, location, date, population, people_fully_vaccinated)
 as
 (
@@ -92,7 +104,7 @@ From PopvsVac
 Group by location, population
 Order by 2 desc;
 
--- Which country is least vaccinated?
+-- Which country is least vaccinated per pupil?
 With PopvsVac (continent, location, date, population, people_fully_vaccinated)
 as
 (
@@ -109,11 +121,8 @@ From PopvsVac
 Group by location, population
 Order by 2 asc;
 
-
-
-
--- Creating View to store data for later visualizations
-
+-- Creating Views to store data for later visualizations
+USE  Portfolio_Project;
 Create View Percent_Population_Vaccinated as 
 With PopvsVac (continent, location, date, population, new_vaccinations, rolling_total_vaccinations)
 as
@@ -130,50 +139,92 @@ order by 2,3
 Select *, (Rolling_total_vaccinations/population) * 100 as Population_Vaxed_Percentage
 From PopvsVac;
 
-Create View Daily_Covid_Cases_by_Country as 
-SELECT Location, date, total_cases, total_deaths, (total_deaths/total_cases) * 100 as Death_Rate
-FROM Portfolio_Project.coviddeaths
-WHERE location = 'United States' and total_cases <> 0
-ORDER BY 1,2;
+CREATE VIEW Daily_Covid_Cases_by_Country AS
+    SELECT 
+        Location,
+        date,
+        total_cases,
+        total_deaths,
+        (total_deaths / total_cases) * 100 AS Death_Rate
+    FROM
+        Portfolio_Project.coviddeaths
+    WHERE
+        location = 'United States'
+            AND total_cases <> 0
+    ORDER BY 1 , 2;
 
-Create View Daily_Infection_Rate_by_Country as 
-SELECT Location, date, population, total_cases, (total_cases/population) * 100 as Population_Infected_Rate
-FROM Portfolio_Project.coviddeaths
-WHERE location = 'United States' and total_cases <> 0
-ORDER BY 1,2;
+CREATE VIEW Daily_Infection_Rate_by_Country AS
+    SELECT 
+        Location,
+        date,
+        population,
+        total_cases,
+        (total_cases / population) * 100 AS Population_Infected_Rate
+    FROM
+        Portfolio_Project.coviddeaths
+    WHERE
+        location = 'United States'
+            AND total_cases <> 0
+    ORDER BY 1 , 2;
 
-Create View Highest_Infected_Countries as
-SELECT Location, population, max(total_cases) as Highest_Infection_Count, Max((total_cases/population)) * 100 as Population_Infected_Rate
-FROM Portfolio_Project.coviddeaths
-GROUP BY Location, Population
-ORDER BY Population_Infected_Rate desc;
+CREATE VIEW Highest_Infected_Countries AS
+    SELECT 
+        Location,
+        population,
+        MAX(total_cases) AS Highest_Infection_Count,
+        MAX((total_cases / population)) * 100 AS Population_Infected_Rate
+    FROM
+        Portfolio_Project.coviddeaths
+    GROUP BY Location , Population
+    ORDER BY Population_Infected_Rate DESC;
 
-Create View Highest_Death_Count as
-SELECT Location, max(total_deaths) as Total_Death_Count
-FROM Portfolio_Project.coviddeaths
-WHERE continent <> ''
-GROUP BY Location
-ORDER BY Total_Death_Count desc;
+CREATE VIEW Highest_Death_Count AS
+    SELECT 
+        Location, MAX(total_deaths) AS Total_Death_Count
+    FROM
+        Portfolio_Project.coviddeaths
+    WHERE
+        continent <> ''
+    GROUP BY Location
+    ORDER BY Total_Death_Count DESC;
 
-Create View Highest_Death_Count as
-SELECT Location, population, max(total_deaths) as total_deaths, max(total_deaths)/population * 1000 as Crude_Death_Rate_per_1k
-FROM Portfolio_Project.coviddeaths
-WHERE continent <> ''
-GROUP BY Location, population
-ORDER BY Crude_Death_Rate_per_1k desc;
+CREATE VIEW Highest_Death_Count AS
+    SELECT 
+        Location,
+        population,
+        MAX(total_deaths) AS total_deaths,
+        MAX(total_deaths) / population * 1000 AS Crude_Death_Rate_per_1k
+    FROM
+        Portfolio_Project.coviddeaths
+    WHERE
+        continent <> ''
+    GROUP BY Location , population
+    ORDER BY Crude_Death_Rate_per_1k DESC;
 
-Create View Highest_Death_Count_by_Continent as
-SELECT location, max(total_deaths) as Total_Death_Count, max(total_deaths)/population * 1000 as Crude_Death_Rate_per_1k
-FROM Portfolio_Project.coviddeaths
-WHERE continent = '' AND location not like "%income%" AND location not like "%world%"
-GROUP BY location, population
-ORDER BY Crude_Death_Rate_per_1k desc;
+CREATE VIEW Highest_Death_Count_by_Continent AS
+    SELECT 
+        location,
+        MAX(total_deaths) AS Total_Death_Count,
+        MAX(total_deaths) / population * 1000 AS Crude_Death_Rate_per_1k
+    FROM
+        Portfolio_Project.coviddeaths
+    WHERE
+        continent = ''
+            AND location NOT LIKE '%income%'
+            AND location NOT LIKE '%world%'
+    GROUP BY location , population
+    ORDER BY Crude_Death_Rate_per_1k DESC;
 
-Create View Overall_Statistics as
-SELECT SUM(new_cases) as total_cases, SUM(new_deaths) as total_deaths, SUM(new_deaths)/sum(new_cases) *100 as Death_Rate
-FROM Portfolio_Project.coviddeaths
-WHERE continent <> '' 
-ORDER BY 1,2;
+CREATE VIEW Overall_Statistics AS
+    SELECT 
+        SUM(new_cases) AS total_cases,
+        SUM(new_deaths) AS total_deaths,
+        SUM(new_deaths) / SUM(new_cases) * 100 AS Death_Rate
+    FROM
+        Portfolio_Project.coviddeaths
+    WHERE
+        continent <> ''
+    ORDER BY 1 , 2;
 
 CREATE VIEW Rolling_Vaccination_Count as
 Select DEATH.continent, DEATH.location, DEATH.date, DEATH.population, VAC.new_vaccinations
@@ -219,3 +270,4 @@ From PopvsVac
 Group by location, population
 Order by 2 asc;
 
+###
